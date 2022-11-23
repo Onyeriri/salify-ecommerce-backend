46708125264ef7e117f7e157ea6b1361396cc40b
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-23 13:00:49 +0000

    create user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,49 @@
-const registerUser = async (req, res) => {
-  res.status(200).send("Register user")
-}
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // validation
+  if (!name, !email, !password) {
+    res.status(400);
+
+    throw new Error('Please fill all the required fields');
+  }
+
+  if (password.length < 6) {
+    res.status(400);
+
+    throw new Error("Password must not be less than six characters")
+  }
+
+  // check if user already exist in the database
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+
+    throw new Error('User already exists, please login.')
+  }
+
+  // create new user
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if (user) {
+    const { _id, name, password, photo, phone, email } = user;
+    res.status(201).json({
+      _id, name, password, phone, photo, email
+    })
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+
+})
 
 module.exports = {registerUser};
