df60b4eba6e655eb53ffebdda4edacd32b21b3a2
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-27 08:46:14 +0000

    Added token to cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,20 +1,19 @@
-const asyncHandler = require('express-async-handler');
-const User = require('../models/userModel');
-const jwt = require('jsonwebtoken');
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-}
-
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   // validation
-  if (!name, !email, !password) {
+  if ((!name, !email, !password)) {
     res.status(400);
 
-    throw new Error('Please fill all the required fields');
+    throw new Error("Please fill all the required fields");
   }
 
   if (password.length < 6) {
@@ -29,30 +28,43 @@ const registerUser = asyncHandler(async (req, res) => {
   if (userExists) {
     res.status(400);
 
-    throw new Error('User already exists, please login.');
+    throw new Error("User already exists, please login.");
   }
-
 
   // create new user
   const user = await User.create({
     name,
     email,
-    password
+    password,
   });
 
   // generate token
   const token = generateToken(user._id);
 
+  // send HTTP-Only cookie
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400),
+    sameSite: "none",
+    secure: true,
+  });
+
   if (user) {
     const { _id, name, password, photo, phone, email } = user;
     res.status(201).json({
-      _id, name, password, phone, photo, email, token
+      _id,
+      name,
+      password,
+      phone,
+      photo,
+      email,
+      token,
     });
   } else {
     res.status(400);
-    throw new Error('Invalid user data');
+    throw new Error("Invalid user data");
   }
-
 });
 
-module.exports = {registerUser};
+module.exports = { registerUser };
