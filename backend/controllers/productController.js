866f9664fc8a52ecda21ea7ebab10c901748b6eb
Onyeriri Victor <viktorchinonso@gmail.com>
2022-12-12 17:59:38 +0000

    created route controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,29 @@
 const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
 
 const createProduct = asyncHandler(async (req, res) => {
-  res.send("create product page...");
+  const { name, sku, category, quantity, price, description } = req.body;
+
+  // validate
+  if (!name || !category || !sku || !quantity || !price || !description) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+
+  // manage image upload
+
+  // create product
+  const product = await Product.create({
+    user: req.user.id,
+    name,
+    sku,
+    category,
+    quantity,
+    price,
+    description,
+  });
+
+  res.status(201).json(product);
 });
 
 module.exports = {
