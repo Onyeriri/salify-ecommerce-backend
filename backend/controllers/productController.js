eaf2eedd2c4d33c2e1d177d4425822cd977c3d19
Onyeriri Victor <viktorchinonso@gmail.com>
2022-12-15 06:24:29 +0000

    completed update functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,6 +3,7 @@ const Product = require("../models/productModel");
 const { fileSizeFormatter } = require("../utils/fileUpload");
 const cloudinary = require("cloudinary").v2;
 
+// create product
 const createProduct = asyncHandler(async (req, res) => {
   const { name, sku, category, quantity, price, description } = req.body;
 
@@ -98,8 +99,71 @@ const deleteProduct = asyncHandler(async (req, res) => {
 
 // update product
 const updateProduct = asyncHandler(async (req, res) => {
-  res.send("update route");
+  const { name, category, quantity, price, description } = req.body;
+
+  const { id } = req.params;
+
+  const product = await Product.findById(id);
+
+  // // if product does not exit
+  if (!product) {
+    res.status(404);
+    throw new Error("User not found!!!");
+  }
+
+  // match product product with user
+  if (product.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  // handle image upload v2
+  let fileData = {};
+  if (req.file) {
+    // save image to cloudinaru
+    let uploadedFile;
+    try {
+      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+        folder: "cloudinary image",
+        resource_type: "image",
+      });
+    } catch (error) {
+      res.status(500);
+      throw new Error("Image upload failed");
+    }
+
+    if (req.file) {
+      fileData = {
+        fileName: req.file.originalname,
+        filePath: uploadedFile.secure_url,
+        fileType: req.file.mimetype,
+        fileSize: fileSizeFormatter(req.file.size, 2),
+      };
+    }
+  }
+
+  // update product
+  const updateProduct = await Product.findByIdAndUpdate(
+    {
+      _id: id,
+    },
+    {
+      name,
+      category,
+      quantity,
+      price,
+      description,
+      image: req.file ? fileData : product.image,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json(updateProduct);
 });
+
 module.exports = {
   createProduct,
   getProducts,
