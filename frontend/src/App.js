6b15790f21ffb805dcff0be5ea70a1a055dfc5d6
Onyeriri Victor <viktorchinonso@gmail.com>
2022-12-20 12:01:19 +0000

    created authentication pages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Forgot from "./pages/auth/Forgot";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Reset from "./pages/auth/Reset";
 import Home from "./pages/Home/Home";
 
 const App = () => {
@@ -7,6 +11,10 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot" element={<Forgot />} />
+          <Route path="/reset-password/:reset-token" element={<Reset />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
